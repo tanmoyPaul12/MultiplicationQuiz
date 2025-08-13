008ea2d9336495e22a24d18a8a74a05e09ed5f17
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-08-13 22:38:28 +0530

    done

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-const num1 = Math.floor(Math.random()*10)
-console.log(num1);
+const num1 = Math.floor(Math.random()*10);
+const num2 = Math.floor(Math.random()*10);
+
+const queEle = document.getElementById("que")
+const inputEle = document.getElementById("input")
+const formEle = document.getElementById("form")
+const scoreEle = document.getElementById("score")
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+    score = 0;
+}
+
+scoreEle.innerText = `Score: ${score}`;
+queEle.innerText = `What is ${num1} Multiply by ${num2}?`
+
+const correctAns = num1 * num2;
+
+const updateBtn = ()=>{
+    const userAns = +inputEle.value;
+    if(userAns===correctAns){
+        score++;
+        updateLocalStorage()
+    }
+    else{
+        score--;
+        updateLocalStorage()
+    }
+}
+
+formEle.addEventListener("submit", updateBtn);
+
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score))
+}
